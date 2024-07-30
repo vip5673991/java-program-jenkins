e479f68c68d64c3d5d088bf2cd74dbfc02b79d03
vip5673991 <vipul.gupta@3ds.com>
2024-07-30 17:07:18 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("this is java program");
+        System.out.println("output from java program");
+        System.out.println(new Date());
+}
+}
